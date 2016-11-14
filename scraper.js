900b763c6b24823544ecb6276448798b5a424196
TaoistBear <originaltaoistbear@gmail.com>
2016-11-14 15:55:41 -0600

    pause for night.  getting there
    another try tomorrow

--- scraper.js
+++ scraper.js
@@ -4,24 +4,19 @@
 // *** REQUIRES & VARIABLES ***
 //******************************************************************************
 
-var $ = require('cheerio');
 var fs = require('fs');
 // Chose x-ray due to pagination versatility and simple user interface.  Many other scrapers had/have mutliple dependancies and API was verbose and arduouse.
 var Xray = require('x-ray');
 var x = Xray();
-var today = new Date();
-
-// Create folder to hold data
-fs.mkdir('../data');
-
-// set basic open/write function in place... using synchronis to keep data consdtant between usages
-fs.openSync('../data/data.csv', 'a', function(err, data) {
-
-});
+var json2csv = require('json2csv');
+var fields = [Title, Price, ImageURL, URL, today];
 
 //******************************************************************************
 // x-ray basics and testing
 //******************************************************************************
+
+//make file for data
+fs.openSync('data.csv', 'a', )
 
 x('http://www.shirts4mike.com/shirts.php', {
    Title: 'img@alt',
@@ -31,23 +26,24 @@ x('http://www.shirts4mike.com/shirts.php', {
 })
   .write('results.json')
 
+fs.readFile('results.json', function(err, data) {
+  if (err) throw err;
+  var today = new Date();
+  fs.openSync('data.csv', 'a', function() {
+    try {
+      json2csv({ data: data, fields: fields });
+      console.log(result);
+    } catch (err) {
+      console.log(err);
+    }
+  })
+});
+
+// for tomorrow>>> try and convert to CSV before calling the open and read fs methods.
+
 //******************************************************************************
 // CREATE FUNCTIONS FOR SCRAPE
 //******************************************************************************
-
-// set basic get request to store json results
-var xhReq = new XMLHttpRequest();
-xhReq.open("GET", results.json, false);
-xhReq.send(null);
-var jsonObject = JSON.parse(xhReq.responseText);
-
-// set basic director for data
-fs.mkdir('../data');
-
-// set basic open/write function in place... using synchronis to keep data consdtant between usages
-fs.openSync('../data/data.csv', '', function(err, data) {
-
-});
 
 //******************************************************************************
 // CREATE FUNCTIONS FOR CVS EXPORT
