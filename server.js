cb39a73e8135570c1cad99c40e3e2642f7f42b7a
TaoistBear <originaltaoistbear@gmail.com>
2016-11-16 13:15:16 -0600

    basic scrape and store complete

--- server.js
+++ server.js
@@ -1,46 +1,68 @@
-//******************************************************************************
-// Launch a web server
-//******************************************************************************
-
 var express = require('express');
 var fs      = require('fs');
 var request = require('request');
 var cheerio = require('cheerio');
 var app     = express();
 
-app.get('/scrape', function(req, res) {
+app.get('/scrape', function(req, res){
+  
+  url = 'http://www.shirts4mike.com/shirt.php?id=101';
 
-  // The URL to scrape from will be http://www.shirts4mike.com/shirts.php
-
-  url = 'http://www.shirts4mike.com/shirts.php?id=101';
-
-  // The structure of our request call
-  // The first parameter is our URL
-  // The call back function takes 3 prameters, and error, response status code and the html
-
-  request(url, function(error, response, html) {
-
-    // First we'll check to make sure no erros occured when making the request
-
-    if(!error) {
-
-      // Next, we'll utilize the cheerio library on the returned html which will essentiall give us jQuery functionality
-
+  request(url, function(error, response, html){
+    if(!error){
       var $ = cheerio.load(html);
 
-      // Fially, we'll define the variable we're going to capture
+      var title, price, imageUrl, sourceUrl, dateTime;
+      var json = { title : "", price : "", imageUrl : "", sourceUrl : "", dateTime : "" };
 
-      var title, price, imageURL, sourceURL, dateTime;
-      var json = { title: "", price: "", imageURL: "", sourceURL: "", dateTime: "" };
+      $('.shirt-picture').filter(function(){
+        var data = $(this);
+        title = data.children().first().children().first().attr('alt');
+        imageUrl = data.children().first().children().first().attr('src');
 
+        json.title = title;
+        json.imageUrl = imageUrl;
+      })
+
+      $('.price').filter(function(){
+        var data = $(this);
+        price = data.text();
+
+        json.price = price;
+      })
+
+      sourceUrl = url;
+
+      json.sourceUrl = sourceUrl;
+
+      // get and create date and time
+      var           today = new Date();
+                       dd = today.getDate(),
+                       mm = today.getMonth()+1,
+                     yyyy = today.getFullYear(),
+                     hour = today.getHours(),
+                   minute = today.getMinutes(),
+                   second = today.getSeconds(),
+            hourFormatted = hour % 12 || 12,
+          minuteFormatted = minute < 10 ? '0' + minute : minute,
+                  morning = hour < 12 ? 'am' : 'pm';
+      if (dd<10) {
+        dd = '0' + dd;
+      }
+      if (mm<10) {
+        mm = '0' + mm;
+      }
+      json.dateTime = mm + '/' + dd + '/' + yyyy + ' ' + hourFormatted + ':' + minuteFormatted + ':' + second + ' ' + morning;
     }
 
-  })
+    fs.writeFile('output.json', JSON.stringify(json, null, 4), function(err){
+      console.log('File successfully written! - Check your project directory for the output.json file');
+    })
 
+    res.send('Check your console!')
+  })
 })
 
 app.listen('8081')
-
 console.log('Magic happens on port 8081');
-
 exports = module.exports = app;
